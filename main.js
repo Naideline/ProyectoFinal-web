999b77e0cf6ace479dd580982f225be22493295c
Naideline <107892053+Naideline@users.noreply.github.com>
2023-11-20 16:29:44 -0600

    Changes

---
+++ main.js
@@ -0,0 +1,9 @@
+function highlightProject(element) {
+    element.style.animation = 'highlight 0.3s ease';
+  }
+  
+  function resetProject(element) {
+    element.style.animation = ''; // Reset animation after it completes
+  }
+  
+
